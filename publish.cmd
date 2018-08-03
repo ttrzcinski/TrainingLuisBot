a0560d989f68e38a0ec4365322c46f2e029e9c48
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-03 15:33:08 +0200

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=trainingluisbot-Web-Deploy.pubxml -p:Password=mbdRH4kwvTLK2llHt99H1a9e0Q8n2c1hxQE4pn2254khFdS0ZCqAHuQTbssT
+
